40e0086e6abf4bf744855b9ac4efbeeebc8a16b4
Shikhar Arora <shikhararora98@gmail.com>
2019-07-24 10:15:02 +0000

    initial

--- routes/addorderRouter.js
+++ routes/addorderRouter.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const router=express.Router()
 
-router.get("/",(req,res,next)=>{
+router.get("/",(req,res)=>{
     // res.send("hello there!")
     res.status(200).json({status:"ok"})
     res.end();
