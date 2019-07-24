0caf9d3f2c0af5e75096982feb38cb5aad1ba779
Shikhar Arora <shikhararora98@gmail.com>
2019-07-24 01:00:42 +0000

    initial

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app=express()
 const orderRouter = require('./routes/orderRouter');
 const addorderRouter = require('./routes/addorderRouter');
 const editorderRouter = require('./routes/editorderRouter');
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.use('/', addorderRouter);
 // app.use('/addorder', orderRouter);
 // app.use('/editorder', editorderRouter);
--- routes/addorderRouter.js
+++ routes/addorderRouter.js
@@ -1,5 +1,6 @@
 const express=require("express")
 const router=express.Router()
+
 router.get("/",(req,res,next)=>{
     res.render("index.html")
     console.log("Hello\n There\n I amm Shikhar")
