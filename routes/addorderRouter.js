3dc5c037c8757157dba15cda30a14bcf7b2f7a06
Shikhar Arora <shikhararora98@gmail.com>
2019-07-24 10:27:02 +0000

    initial

--- routes/addorderRouter.js
+++ routes/addorderRouter.js
@@ -3,7 +3,8 @@ const router=express.Router()
 
 router.get("/",(req,res)=>{
     // res.send("hello there!")
-    res.status(200).json({status:"ok"})
+    // res.status(200).json({status:"ok"})
+    res.sendStatus(200)
     res.end();
     console.log("Hello\n There\n I amm Shikhar")
     console.log(req.body)
