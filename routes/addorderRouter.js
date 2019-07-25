dea102ca125cd7ed58fa5cf3e2d681dc406e1d71
Shikhar Arora <33093435+shikhar98@users.noreply.github.com>
2019-07-25 11:02:01 +0000

    Update addorderRouter.js

--- routes/addorderRouter.js
+++ routes/addorderRouter.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const router=express.Router()
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:admin@sforums-zq5nz.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://admin:admin@sforums-zq5nz.mongodb.net/orders?retryWrites=true&w=majority', {useNewUrlParser: true});
 orders=require("../Controllers/orders");
 var bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({extended: true}));
